a3bef97402d302bb3d38335ffa32693d482ea4ee
DenysiukD <gita.antifem@gmail.com>
2025-11-17 19:05:29 +0100

    starting api code

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ card.forEach(function(element, index){
         overlay.classList.add("modal-open")
     })
 })
+
+const fetchPostsBtn = document.querySelector(".btn");
+const postList = document.querySelector(".posts");
+
+fetchPostsBtn.addEventListener("click", async () => {
+  try {
+    const posts = await fetchPosts();
+    renderPosts(posts);
+  } catch (error) {
+    console.log(error);
+  }
+});
+const fetchUsers = async () => {
+ try {
+   const response = await fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=dCkG1wKu6R5quyoXGoZgyyMJHge0r4FS&size=20&page=1");
+   const users = await response.json();
+   users._embedded.events.forEach(element => {
+    console.log(element);
+    
+   });
+ } catch (error) {
+   console.log(error.message);
+ }
+};
+
+fetchUsers();
+
+
+
